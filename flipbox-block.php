72cb6cf7ee16744f88dd45ff1d5e1e9e14b793c9
shimotmk <shimotmk1104@gmail.com>
2022-03-19 19:33:20 +0900

    upddate version 0.1.16

--- flipbox-block.php
+++ flipbox-block.php
@@ -5,7 +5,7 @@
  * Description:       Flip Box Block is a block plugin for the WordPress block editor that allows you to freely place blocks in front and behind.
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           0.1.15
+ * Version:           0.1.16
  * Stable tag:        0.1.6
  * Author:            Tomoki Shimomura
  * Author URI:        https://flip-box-block.shimomuratomoki.com/
